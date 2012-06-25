56d9ac7d982f642bfae33ea73f71bbf0062b00bb
Thomas Christensen <thomas@chrstnsn.dk>
2012-06-26 01:01:22 +0200

    more fooling around

--- src/clj_euler/core.clj
+++ src/clj_euler/core.clj
@@ -2,19 +2,17 @@
   (:use clj-euler.primes))
 
 (defn- m
-  ([]  (m 1000))
-  ([n]
-     (println (str "Last primes before "
-                   n
-                   ": "
-                   (last (primes-below n))))))
+  ([] (m 1000))
+  ([n] (println
+        (str "Last primes before "
+             n
+             ": "
+             (last (primes-below n))))))
 
 (defn- s->i [s]
   (Integer/parseInt s 10))
 
-
 (defn -main
   ([] (m))
   ([arg] (let [ia (s->i arg)] (m ia)))
-  ([an-arg & other-args] (println "invalid number of arguments..."))
-  )
+  ([an-arg & other-args] (println "invalid number of arguments...")))
