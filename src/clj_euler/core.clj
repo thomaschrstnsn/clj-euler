9d93c492d4d15138bb6e07f95947412a7f7bdcd1
Thomas Christensen <thomas@chrstnsn.dk>
2012-07-11 15:08:33 +0200

    dryrun switch

--- src/clj_euler/core.clj
+++ src/clj_euler/core.clj
@@ -1,5 +1,7 @@
 (ns clj-euler.core
   (:use [clj-euler.utils])
+  (:use [clojure.set :only [difference]])
+  (:use [clojure.string :only [join]])
   (:use [clojure.tools.namespace :only [find-ns-decls-on-classpath]])
   (:use [clojure.tools.cli :only [cli]]))
 
@@ -25,12 +27,14 @@
   (try (fn)
        (catch Exception e (str "Caught exception " (.getMessage e)))))
 
-(defn- time-fn [ {:keys [timing]} {:keys [fn problem]}]
+(defn- time-fn [{:keys [timing dryrun fname]}
+                {:keys [fn problem-num]}]
   (let [timer (if timing
                 #(time (%))
                 #(%))]
-    (println (str "-> problem: " problem))
-    (println (str "==> " (timer (partial ex-wrap fn))))))
+    (println (str "-> " fname ": " problem-num))
+    (when-not dryrun
+      (println (str "==> " (timer (partial ex-wrap fn)))))))
 
 (defn- fn->problem-number [fn]
   (->> fn
@@ -40,17 +44,13 @@
 
 (defn- fn->problem-map [fn]
   {:fn fn
-   :problem (fn->problem-number fn)})
+   :problem-num (fn->problem-number fn)})
 
-(defn- fns-in-problem-namespaces [fn-symbol num-predicate options]
+(defn- problem-maps-from-namespaces [fn-symbol num-predicate]
   (->> (problem-namespaces)
        (map (partial fn-in-ns fn-symbol))
        (filter (complement nil?))
-       (map fn->problem-map)
-       (filter (comp num-predicate :problem))
-       (map (partial time-fn options))
-       (doall))
-  nil)
+       (map fn->problem-map)))
 
 (defn- parse-problem-args! [args]
   (try (doall (map s->n args))
@@ -59,23 +59,34 @@
            (println "Failed to parse arguments for problem numbers")
            (System/exit -1)))))
 
-(defn- run [options]
-  (let [fname (if (options :example) 'example 'problem)
-        problem-set (set (options :problems))
-        num-predicate (if (options :all)
-                        (fn [n] true)
-                        (fn [n] (contains? problem-set n)))]
-    (fns-in-problem-namespaces fname num-predicate options)))
+(defn- run [{:keys [fname problem-set all] :as options}]
+  (let [num-predicate     (if all
+                            (fn [n] true)
+                            (fn [n] (contains? problem-set n)))
+        all-problem-maps  (problem-maps-from-namespaces fname num-predicate)
+        specified-missing (difference problem-set
+                                      (set (map :problem-num all-problem-maps)))
+        filtered-pms      (filter (comp num-predicate :problem-num) all-problem-maps)
+        time-fns          (map (partial time-fn options) filtered-pms)]
+    (when-not (empty? specified-missing)
+      (println "Specified problems not implemented: " (join ", " specified-missing)))
+    (doall time-fns)
+    nil))
+
+(defn- enrich-options [{:keys [example] :as options} specified-problems]
+  (into options {:fname (if example 'example 'problem)
+                 :problem-set (set specified-problems)}))
 
 (defn -main [& args]
   (let [[options extra banner] (cli args
                                     ["-e" "--example" "Only run the examples (not the full problem)." :flag true :default false]
                                     ["-a" "--all" "Run all." :flag true :default false]
-                                    ["-t" "--timing" "Do timing." :flag true :default true])
-        specified-problems (parse-problem-args! extra)]
+                                    ["-t" "--timing" "Do timing." :flag true :default true]
+                                    ["-d" "--dryrun" "Do not actually run anything, just list what would be done" :flag true :default false])
+        specified-problems     (parse-problem-args! extra)]
     (when-not (or (options :all) ((complement empty?) specified-problems))
-      (println "[switches] ([problem number]*)")
+      (println "clj-euler [switches] ([problem number]*)")
       (println "")
       (println banner)
       (System/exit 0))
-    (run (assoc options :problems specified-problems))))
+    (run (enrich-options options specified-problems))))
