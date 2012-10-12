18e16ddb0f487ffe70826732a8053ba485edc69b
Thomas Christensen <thomas@chrstnsn.dk>
2012-10-12 14:39:23 +0200

    problem 8

--- src/clj_euler/utils.clj
+++ src/clj_euler/utils.clj
@@ -1,7 +1,10 @@
 (ns clj-euler.utils)
 
-(defn s->n [s]
+(defn s->n [^String s]
   (Integer/parseInt s 10))
+
+(defn c->n [^Character c]
+  (Integer/parseInt (str c) 10))
 
 (defn mod-zero? [n d]
   (zero? (mod n d)))
