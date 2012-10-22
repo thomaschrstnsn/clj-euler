31259d2fc4f533455452da4831f9bd9bb298a95a
Thomas Christensen <thomas@chrstnsn.dk>
2012-10-22 22:51:08 +0200

    imperative and faster (not c-speed though) solution for prob 14

--- src/clj_euler/problem014.clj
+++ src/clj_euler/problem014.clj
@@ -50,3 +50,23 @@
          (apply sorted-map)
          (last)
          (second))))
+
+(defn- ^long imp-helper [^long j ^long this_terms]
+  (if (not= j 1)
+    (cond
+     (= 0 (rem j 2)) (recur (bit-shift-right j 1) (unchecked-inc this_terms))
+     :else (recur (unchecked-inc (unchecked-multiply j 3)) (unchecked-inc this_terms)))
+    this_terms))
+
+(defn
+  ^{:solution {:expected 837799 :arguments [1000000]}}
+  ^long imperative [^long limit]
+  (loop [longest 0
+         terms 0
+         i 1]
+    (if (<= i limit)
+      (let [this_terms (imp-helper i 1)]
+        (if (< terms this_terms)
+          (recur i this_terms (unchecked-inc i))
+          (recur longest terms (unchecked-inc i))))
+      longest)))
